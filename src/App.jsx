15a647aca9268aeac5bc4e9e3f7c43043d7d31ff
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-10-28 13:55:34 +0300

    add Second and Third Section components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Alert from "./components/Alert";
 import FirstSection from "./components/FirstSection";
 import Header from "./components/Header";
+import SecondSection from "./components/SecondSection";
+import ThirdSection from "./components/ThirdSection";
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Header />
       <Alert />
       <FirstSection />
+      <SecondSection />
+      <ThirdSection />
     </>
   );
 }
