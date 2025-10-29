71782d30c53d87bedc5fca2a8f9adeda8b752f56
Desalegn Personal <desalegntamirateshetu@gmail.com>
2025-10-29 13:03:44 +0300

    put Third and Fourth Section on separate folder along with their own modular css

--- src/components/FourthSection/FourthSection.jsx
+++ src/components/FourthSection/FourthSection.jsx
@@ -1,17 +1,23 @@
+import styles from "./fourthsection.module.css";
 import React from "react";
 
 export default function FourthSection() {
   return (
-    <section className="fourth-heghlight-wrapper">
+    <section
+      className={`${styles["fourth-heghlight-wrapper"]} fourth-heghlight-wrapper}`}
+    >
       <div className="container-fluid">
         <div className="row">
-          <div className="left-side-wrapper col-sm-12 col-md-6">
-            <div className="left-side-container">
+          <div className={`col-sm-12 col-md-6 left-side-wrapper`}>
+            <div
+              className={`${styles["left-side-container"]} left-side-container`}
+            >
               <div className="title-wraper">iPhone 11</div>
-              <div className="description-wraper">
+              <div className="description-wrapper">
                 Just the right amount of everything.
               </div>
-              <div className="price-wrapper">
+
+              <div className={styles["price-wrapper"]}>
                 From $18.70/mo. or $499 with trade‑in.<sup>1</sup>
               </div>
 
@@ -27,13 +33,16 @@ export default function FourthSection() {
               </div>
             </div>
           </div>
-          <div className="right-side-wrapper col-sm-12 col-md-6">
-            <div className="right-side-container">
+          <div className={`col-sm-12 col-md-6 right-side-wrapper`}>
+            <div
+              className={`${styles["right-side-container"]} right-side-container`}
+            >
               <div className="title-wraper white">
                 Get the latest CDC response to COVID-19.
               </div>
-
-              <div className="links-wrapper white">
+              <div
+                className={`links-wrapper ${styles["links-wrapper"]} ${styles.white}`}
+              >
                 <ul>
                   <li>
                     <a href="">Watch the PSA</a>
