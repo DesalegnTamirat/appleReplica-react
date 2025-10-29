21d14e224e71d7b59b69bb6e3320037186db836c
Desalegn Personal <desalegntamirateshetu@gmail.com>
2025-10-29 20:19:55 +0300

    group secion together in one component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import Alert from "./components/Alert/Alert";
-import FifthSection from "./components/FifthSection/FifthSection";
-import FirstSection from "./components/FirstSection/FirstSection";
 import Footer from "./components/Footer/Footer";
-import FourthSection from "./components/FourthSection/FourthSection";
 import Header from "./components/Header/Header";
-import SecondSection from "./components/SecondSection/SecondSection";
-import SixthSection from "./components/SixthSection/SixthSection";
-import ThirdSection from "./components/ThirdSection/ThirdSection";
+import Main from "./components/Main/Main";
 
 export default function App() {
   return (
     <>
       <Header />
       <Alert />
-      <FirstSection />
-      <SecondSection />
-      <ThirdSection />
-      <FourthSection />
-      <FifthSection />
-      <SixthSection />
+      <Main />
       <Footer />
     </>
   );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,19 @@
+import FirstSection from '../FirstSection/FirstSection'
+import SecondSection from '../SecondSection/SecondSection'
+import ThirdSection from '../ThirdSection/ThirdSection'
+import FourthSection from '../FourthSection/FourthSection'
+import FifthSection from '../FifthSection/FifthSection'
+import SixthSection from '../SixthSection/SixthSection'
+
+export default function Main() {
+  return (
+    <main>
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <FourthSection />
+      <FifthSection />
+      <SixthSection />
+    </main>
+  )
+}
